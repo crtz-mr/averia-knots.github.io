1cc24e9e865dac5701f046c7dd2ac4d9862f38f9
avjria <cortez.mariaavjcortez@gmail.com>
2026-03-18 21:31:59 +0200

    Implement comment addition and display functionality
    Added functions to add and display comments using local storage.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function addComment() {
+  let name = document.getElementById("name").value.trim();
+  let comment = document.getElementById("comment").value.trim();
+  if (!name || !comment) return alert("Please enter both name and comment");
+  let comments = JSON.parse(localStorage.getItem("comments")) || [];
+  comments.push({name, comment});
+  localStorage.setItem("comments", JSON.stringify(comments));
 
+  document.getElementById("name").value = '';
+  document.getElementById("comment").value = '';
+
+displayComments();
+  
+}
+
+function displayComments() {
+  let list = document.getElementById("commentList");
+  list.innerHTML = '';
+  let comments = JSON.parse(localStorage.getItem("comments")) || [];
+  comments.forEach(c => {
+    let div = document.createElement("div");
+    div.className = "comment";
+    div.innerHTML = `<b>${c.name}</b>: ${c.comment}`;
+    list.appendChild(div);
+  });
+}
+
+displayComments();
